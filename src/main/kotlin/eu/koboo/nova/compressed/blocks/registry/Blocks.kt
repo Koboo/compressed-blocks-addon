aa057df3da23d43c7de2f215c3eef1b241612d9d
Koboo <admin@koboo.eu>
2023-10-03 11:10:10 +0200

    Update textures + Add sand (1x, 2x, 3x, 4x) + cobbled_deepslate (1x, 2x, 3x, 4x)

--- src/main/kotlin/eu/koboo/nova/compressed/blocks/registry/Blocks.kt
+++ src/main/kotlin/eu/koboo/nova/compressed/blocks/registry/Blocks.kt
@@ -20,11 +20,37 @@ object Blocks : BlockRegistry by CompressedBlocksAddon.registry {
             SoundGroup.STONE,
             Material.COBBLESTONE
     )
+    private val OPTIONS_COBBLED_DEEPSLATE = BlockOptions(
+            3.5,
+            VanillaToolCategories.PICKAXE,
+            VanillaToolTiers.WOOD,
+            true,
+            SoundGroup.STONE,
+            Material.COBBLED_DEEPSLATE
+    )
+    private val OPTIONS_SAND = BlockOptions(
+            1.5,
+            VanillaToolCategories.SHOVEL,
+            VanillaToolTiers.WOOD,
+            false,
+            SoundGroup.SAND,
+            Material.SAND
+    )
+
+    val COBBLED_DEEPSLATE_1X = block("compressed_cobbled_deepslate_1x").blockOptions(OPTIONS_COBBLED_DEEPSLATE).register()
+    val COBBLED_DEEPSLATE_2X = block("compressed_cobbled_deepslate_2x").blockOptions(OPTIONS_COBBLED_DEEPSLATE).register()
+    val COBBLED_DEEPSLATE_3X = block("compressed_cobbled_deepslate_3x").blockOptions(OPTIONS_COBBLED_DEEPSLATE).register()
+    val COBBLED_DEEPSLATE_4X = block("compressed_cobbled_deepslate_4x").blockOptions(OPTIONS_COBBLED_DEEPSLATE).register()
 
     val COBBLESTONE_1X = block("compressed_cobblestone_1x").blockOptions(OPTIONS_STONE).register()
     val COBBLESTONE_2X = block("compressed_cobblestone_2x").blockOptions(OPTIONS_STONE).register()
     val COBBLESTONE_3X = block("compressed_cobblestone_3x").blockOptions(OPTIONS_STONE).register()
     val COBBLESTONE_4X = block("compressed_cobblestone_4x").blockOptions(OPTIONS_STONE).register()
+
+    val SAND_1X = block("compressed_sand_1x").blockOptions(OPTIONS_SAND).register()
+    val SAND_2X = block("compressed_sand_2x").blockOptions(OPTIONS_SAND).register()
+    val SAND_3X = block("compressed_sand_3x").blockOptions(OPTIONS_SAND).register()
+    val SAND_4X = block("compressed_sand_4x").blockOptions(OPTIONS_SAND).register()
 
     val STONE_1X = block("compressed_stone_1x").blockOptions(OPTIONS_STONE).register()
     val STONE_2X = block("compressed_stone_2x").blockOptions(OPTIONS_STONE).register()
--- src/main/kotlin/eu/koboo/nova/compressed/blocks/registry/Items.kt
+++ src/main/kotlin/eu/koboo/nova/compressed/blocks/registry/Items.kt
@@ -7,10 +7,21 @@ import xyz.xenondevs.nova.initialize.Init
 @Init
 object Items : ItemRegistry by CompressedBlocksAddon.registry {
 
+
+    val COBBLED_DEEPSLATE_1X = registerItem(Blocks.COBBLED_DEEPSLATE_1X)
+    val COBBLED_DEEPSLATE_2X = registerItem(Blocks.COBBLED_DEEPSLATE_2X)
+    val COBBLED_DEEPSLATE_3X = registerItem(Blocks.COBBLED_DEEPSLATE_3X)
+    val COBBLED_DEEPSLATE_4X = registerItem(Blocks.COBBLED_DEEPSLATE_4X)
+
     val COBBLESTONE_1X = registerItem(Blocks.COBBLESTONE_1X)
     val COBBLESTONE_2X = registerItem(Blocks.COBBLESTONE_2X)
     val COBBLESTONE_3X = registerItem(Blocks.COBBLESTONE_3X)
     val COBBLESTONE_4X = registerItem(Blocks.COBBLESTONE_4X)
+
+    val SAND_1X = registerItem(Blocks.SAND_1X)
+    val SAND_2X = registerItem(Blocks.SAND_2X)
+    val SAND_3X = registerItem(Blocks.SAND_3X)
+    val SAND_4X = registerItem(Blocks.SAND_4X)
 
     val STONE_1X = registerItem(Blocks.STONE_1X)
     val STONE_2X = registerItem(Blocks.STONE_2X)
